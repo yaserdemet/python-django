2231b3c403260d38a5c7be4963a1d03dbd22b3ad
yaser <ddemety@gmail.com>
2022-03-27 13:21:35 +0300

    added | randit_guess_number

--- 005-randit_guess_number.py
+++ 005-randit_guess_number.py
@@ -1,3 +1,7 @@
+
+#  Write a pyhton code, which is a number guessing game that tries to find the number that will be randomly ,  
+# selected from the number sequence from 1 to 100. and indicate in the code that each user has the right to guess 5, deduct 1 right for each wrong guess
+
 import random  
 x = random.randint(1,100)
 
