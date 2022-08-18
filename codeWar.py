f814d20e5b30303e41c60b4335f5cef2371bed2d
yaser <ddemety372@gmail.com>
2022-08-18 23:11:16 +0300

    codewars

--- codeWar.py
+++ codeWar.py
@@ -1,8 +1,19 @@
 #Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
-def func(string):
-    empty = []
-    [empty.append(1) if int(i) > 5 else empty.append(0) for i in string]
-    return "".join(str(i) for i in empty)
+# def func(string):
+#     empty = []
+#     [empty.append(1) if int(i) > 5 else empty.append(0) for i in string]
+#     return "".join(str(i) for i in empty)
 
-func("123456789")
+# func("123456789")
+
+
+
+# ? Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+vovels = ['a', 'e', 'i', 'o', 'u']
+
+def remove_vovels(string):
+    return "".join(i for i in string if i not in vovels)
+
+remove_vovels("hello mother fucker")
