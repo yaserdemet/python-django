f158151268be21abeef431656772baf8c24ea0f6
yaserdemet <ddemety372@gmail.com>
2022-08-27 13:40:30 +0300

    Update codeWar.py

--- pyhon-algorithm/codeWar.py
+++ pyhon-algorithm/codeWar.py
@@ -83,3 +83,25 @@ def combine(*args) :
     return my_new
 
 print(combine({"a": 10,"b": 20, "c": 30} , {"a": 3, "c": 6, "d": 3 , "b": 10}))
+
+
+
+# ? ==============================================
+
+
+# You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+def get_middle(s):
+    empty = ""
+    for i in s:
+        if len(s) % 2  == 0:
+            empty += s[int(len(s) / 2 -1) ] + s[int(len(s) / 2 )]
+            
+        elif len(s) % 2 == 1:
+            empty += s[int(len(s) / 2  )]
+        
+        elif len(s) ==1:
+            empty += s[0]
+            
+        return empty
+get_middle("testing")
